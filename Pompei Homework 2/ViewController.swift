3bff942e15bb9f445b0524ed430f6a5d73206f3c
Michael Pompei <m.pompei@setonhill.edu>
2020-02-24 12:20:23 -0500

    Initial Commit

---
+++ Pompei Homework 2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pompei Homework 2
+//
+//  Created by Michael David Pompei on 2/24/20.
+//  Copyright © 2020 Michael David Pompei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
